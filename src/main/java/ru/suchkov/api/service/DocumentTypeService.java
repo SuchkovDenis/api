3436633474d4ad4355c03a27d066f53778600c04
Suchkov Denis <ny1828@yandex.ru>
2019-04-04 20:05:58 +0300

    Добавлены заготовки сервисов и их реализаций для entity классов

---
+++ src/main/java/ru/suchkov/api/service/DocumentTypeService.java
@@ -0,0 +1,4 @@
+package ru.suchkov.api.service;
+
+public interface DocumentTypeService {
+}
---
+++ src/main/java/ru/suchkov/api/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.suchkov.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+}
