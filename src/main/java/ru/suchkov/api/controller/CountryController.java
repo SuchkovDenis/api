f321238a842e3b78b5a88ec60b46668313ae2b28
Suchkov Denis <ny1828@yandex.ru>
2019-04-05 15:00:24 +0300

    Реализованы классы контроллеры обработки запросов на получение списка типов документов и стран

--- src/main/java/ru/suchkov/api/controller/CountryController.java
+++ src/main/java/ru/suchkov/api/controller/CountryController.java
@@ -1,10 +1,15 @@
 package ru.suchkov.api.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.suchkov.api.service.CountryService;
+import ru.suchkov.api.view.CountryView;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -18,5 +23,11 @@ public class CountryController {
     @Autowired
     public CountryController(CountryService countryService) {
         this.countryService = countryService;
+    }
+
+    @ApiOperation(value = "Получить список всех стран", httpMethod = "GET")
+    @GetMapping("/countries")
+    public List<CountryView> persons() {
+        return countryService.countries();
     }
 }
--- src/main/java/ru/suchkov/api/controller/DocumentTypeController.java
+++ src/main/java/ru/suchkov/api/controller/DocumentTypeController.java
@@ -1,10 +1,15 @@
 package ru.suchkov.api.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.suchkov.api.service.DocumentTypeService;
+import ru.suchkov.api.view.DocumentTypeView;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -18,5 +23,11 @@ public class DocumentTypeController {
     @Autowired
     public DocumentTypeController(DocumentTypeService documentTypeService) {
         this.documentTypeService = documentTypeService;
+    }
+
+    @ApiOperation(value = "Получить список всех типов документов", httpMethod = "GET")
+    @GetMapping("/docs")
+    public List<DocumentTypeView> persons() {
+        return documentTypeService.documentTypes();
     }
 }
