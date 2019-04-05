fb211ce08fa8ea1a9d57757aaeeb6281875fda68
Suchkov Denis <ny1828@yandex.ru>
2019-04-05 15:51:42 +0300

    Изменены модификаторы доступа на private для DocumentTypeView и CountryView

--- src/main/java/ru/suchkov/api/view/CountryView.java
+++ src/main/java/ru/suchkov/api/view/CountryView.java
@@ -1,10 +1,12 @@
 package ru.suchkov.api.view;
 
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 
 @ApiModel(description = "Страна")
+@Data
 public class CountryView {
 
-    public String name;
-    public String code;
+    private String name;
+    private String code;
 }
