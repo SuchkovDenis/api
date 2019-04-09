ad89d1468eff8bb26055c9334465e7a4bd1a0331
Suchkov Denis <ny1828@yandex.ru>
2019-04-09 20:22:11 +0300

    Реализован метод OrganizationView organization(@PathVariable long id)

--- src/main/java/ru/suchkov/api/controller/OrganizationController.java
+++ src/main/java/ru/suchkov/api/controller/OrganizationController.java
@@ -1,10 +1,17 @@
 package ru.suchkov.api.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.suchkov.api.service.OrganizationService;
+import ru.suchkov.api.view.DocumentTypeView;
+import ru.suchkov.api.view.OrganizationView;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -18,5 +25,11 @@ public class OrganizationController {
     @Autowired
     public OrganizationController(OrganizationService organizationService) {
         this.organizationService = organizationService;
+    }
+
+    @ApiOperation(value = "Получить организацию по id", httpMethod = "GET")
+    @GetMapping("/organization/{id}")
+    public OrganizationView organization(@PathVariable long id) {
+        return organizationService.getOrganization(id);
     }
 }
