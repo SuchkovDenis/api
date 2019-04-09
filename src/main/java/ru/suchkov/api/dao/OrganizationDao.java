815911a9691e65f4e4234b739fb7899a1759718e
Suchkov Denis <ny1828@yandex.ru>
2019-04-09 18:32:55 +0300

    Добавлены методы в OrganizationDao

--- src/main/java/ru/suchkov/api/dao/OrganizationDao.java
+++ src/main/java/ru/suchkov/api/dao/OrganizationDao.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.suchkov.api.model.Organization;
 
+import java.util.List;
+
 @Repository
 public interface OrganizationDao extends CrudRepository<Organization, Long> {
+
+    List<Organization> findAll();
+
+    Organization findOrganizationById(long id);
 }
--- src/main/java/ru/suchkov/api/service/OrganizationService.java
+++ src/main/java/ru/suchkov/api/service/OrganizationService.java
@@ -1,4 +1,18 @@
 package ru.suchkov.api.service;
 
+import ru.suchkov.api.model.Organization;
+import ru.suchkov.api.view.OrganizationView;
+
+import java.util.List;
+
 public interface OrganizationService {
+
+    List<OrganizationView> getOrganizations();
+
+    OrganizationView getOrganization(long id);
+
+    OrganizationView updateOrganization(Organization organization);
+
+    OrganizationView saveOrganization(Organization organization);
+
 }
