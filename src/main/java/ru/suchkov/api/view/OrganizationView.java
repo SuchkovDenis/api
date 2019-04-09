948ea4a925a541e123bd18ae2b4e65eb06c5aa4f
Suchkov Denis <ny1828@yandex.ru>
2019-04-09 20:20:38 +0300

    Реализован класс OrganizationView.java

--- src/main/java/ru/suchkov/api/view/OrganizationView.java
+++ src/main/java/ru/suchkov/api/view/OrganizationView.java
@@ -1,7 +1,20 @@
 package ru.suchkov.api.view;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 
 @ApiModel(description = "Организация")
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrganizationView {
+
+    private String id;
+    private String name;
+    private String fullName;
+    private String inn;
+    private String kpp;
+    private String address;
+    private String phone;
+    private String isActive;
 }
